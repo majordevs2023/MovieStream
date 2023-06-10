735ba6cd50d04bd5e0b9d7f4376adfe84a5ddbfa
majordevs2023 <majordevs2023@gmail.com>
2023-06-10 13:29:06 +0300

    update

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,24 +4,25 @@ rootProject.name = "CloudstreamPlugins"
 // to the includes below.
 
 // Plugins are included like this
-//val disabled = listOf<String>(
-//    "KisskhProvider", "Topdocumentaryfilms", "Tvtwofourseven",
+//val enabled = listOf<String>(
+//   "KisskhProvider", "Topdocumentaryfilms", "Tvtwofourseven",
 //)
-
-val disabled = listOf<String>(
-)
-File(rootDir, ".").eachDir { dir ->
-    if (!disabled.contains(dir.name) && File(dir, "build.gradle.kts").exists()) {
-        include(dir.name)
-    }
-}
-
-fun File.eachDir(block: (File) -> Unit) {
-    listFiles()?.filter { it.isDirectory }?.forEach { block(it) }
-}
+//
+//val disabled = listOf<String>(
+//)
+//File(rootDir, ".").eachDir { dir ->
+//    if (!disabled.contains(dir.name) && File(dir, "build.gradle.kts").exists()) {
+//        include(dir.name)
+//    }
+//}
+//
+//fun File.eachDir(block: (File) -> Unit) {
+//    listFiles()?.filter { it.isDirectory }?.forEach { block(it) }
+//}
 
 
 // To only include a single project, comment out the previous lines (except the first one), and include your plugin like so:
 // include("PluginName")
-//include(":WcofunProvider")
-include(":AmharicMovieProvier")
+include(":Topdocumentaryfilms")
+include(":SuperStream")
+include(":AsianLoadProvider")
