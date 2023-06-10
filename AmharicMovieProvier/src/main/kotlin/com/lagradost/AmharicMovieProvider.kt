47dbb3b55f144febdf897a5965dede8d237b2853
majordevs2023 <majordevs2023@gmail.com>
2023-06-10 12:54:28 +0300

    updte

--- AmharicMovieProvier/src/main/kotlin/com/lagradost/AmharicMovieProvider.kt
+++ AmharicMovieProvier/src/main/kotlin/com/lagradost/AmharicMovieProvider.kt
@@ -15,45 +15,24 @@ class AmharicMovieProvider : MainAPI() {
 
 
     override suspend fun getMainPage(page: Int, request : MainPageRequest): HomePageResponse {
-        val items = ArrayList<HomePageList>()
-        val soup = app.get(mainUrl).document
-        val videoItems = soup .select("li.col-xs-6")
-        try {
-            val elements: List<SearchResponse> = videoItems.map {
-                val titleElement = it.selectFirst("h3 a")
-                val title = titleElement?.text()
-                val href = titleElement?.attr("href")
-                val thumbnailElement = it.selectFirst("img")
-                val thumbnailUrl = thumbnailElement?.attr("data-echo")
-                val link = when (href != null) {
-                    true -> fixUrl(href)
-                    false -> ""
-                }
-                title?.let { it1 ->
-                    MovieSearchResponse(
-                        it1,
-                        link,
-                        this.name,
-                        TvType.Movie,
-                        thumbnailUrl,
-                        0,
-                        null,
-                    )
-                }!!
-            }
-            items.add(
-                HomePageList(
-                    name, elements
-                )
-            )
+//        val items = ArrayList<HomePageList>()
+//        val soup = app.get(mainUrl).document
+//        val videoItems = soup .select("li.col-xs-6")
+//        try {
+//            val elements: List<SearchResponse> = videoItems.map {
+//                val titleElement = it.selectFirst("h3 a")
+//                val title = titleElement?.text()
+//                val href = titleElement?.attr("href")
+//                val thumbnailElement = it.selectFirst("img")
+//                val thumbnailUrl = thumbnailElement?.attr("data-echo")
+//
+//
+//        }catch (e:Exception){
+//            logError(e)
+//        }
+//
+//    }
 
-        }catch (e:Exception){
-            logError(e)
-        }
-
-        return HomePageResponse(items)
     }
-
-
 
 }
