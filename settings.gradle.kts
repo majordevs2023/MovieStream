0a9e1abe9176f13bbafa2e7660beab8ca2bfa8a2
majordevs2023 <majordevs2023@gmail.com>
2023-06-11 20:52:51 +0300

    update

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,12 +1,12 @@
 rootProject.name = "CloudstreamPlugins"
 
- This file sets what projects are included. Every time you add a new project, you must add it
- to the includes below.
+// This file sets what projects are included. Every time you add a new project, you must add it
+// to the includes below.
 
- Plugins are included like this
-val enabled = listOf<String>(
-   "KisskhProvider", "Topdocumentaryfilms", "Tvtwofourseven",
-)
+// Plugins are included like this
+//val enabled = listOf<String>(
+//   "KisskhProvider", "Topdocumentaryfilms", "Tvtwofourseven",
+//)
 
 val disabled = listOf<String>(
 )
@@ -25,6 +25,6 @@ fun File.eachDir(block: (File) -> Unit) {
 // include("PluginName")
 
 //working dircotry
-include(":Topdocumentaryfilms")
-include(":SuperStream")
-include(":VidstreamBundle")
+//include(":Topdocumentaryfilms")
+//include(":SuperStream")
+//include(":VidstreamBundle")
